4acf49228d7dfb6c4f45688870e07af5d5935126
dcuello <damcuello1@gmail.com>
2023-11-06 23:27:17 -0300

    img

--- JavaScript/menus.js
+++ JavaScript/menus.js
@@ -3,7 +3,7 @@ const menus = [
         id: 1,
         nombre: "Hamburguesa con queso",
         precio: 150,
-        imagen : "../imagen/hamburguesa-con-queso.png",
+        imagen : "./imagen/hamburguesa-con-queso.png",
     },
 
 
@@ -11,34 +11,34 @@ const menus = [
         id: 2,
         nombre: "Pollo Crispy con cheddar",
         precio: 150,
-        imagen: "../imagen/pollo-crispy.png",
+        imagen: "./imagen/pollo-crispy.png",
     },
 
     {
         id: 3,
         nombre: "Papas Fritas grandes",
         precio: 50,
-        imagen: "../imagen/papas.png",
+        imagen: "./imagen/papas.png",
     },
 
     {
         id: 4,
         nombre: "Vaso de gaseosa grande",
         precio: 75,
-        imagen: "../imagen/gaseosa.png",
+        imagen: "./imagen/gaseosa.png",
     },
 
     {
         id: 5,
         nombre: "Hamburguesa doble con cheddar y panceta",
         precio: 350,
-        imagen: "../imagen/doble-con-cheddar.png",
+        imagen: "./imagen/doble-con-cheddar.png",
     },
 
     {
         id: 6,
         nombre: "Wrap de vegetales",
         precio: 100,
-        imagen: "../imagen/wrap.png",
+        imagen: "./imagen/wrap.png",
     },
 ];
